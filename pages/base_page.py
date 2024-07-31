555e378c70fe6eb71874818bc375cb97db7247e8
DimanObiVan <cyiiepmehdiman@gmail.com>
2024-07-31 09:31:43 +0300

    New test should_see_login_link

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,6 @@ import pytest
 import time
 from selenium.common.exceptions import NoSuchElementException
 
-<<<<<<< HEAD
 class BasePage():
 
     def __init__(self, browser, url):
@@ -13,14 +12,4 @@ class BasePage():
         
     def open(self):
         self.browser.get(self.url)
-=======
-link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
->>>>>>> 033e072739ada8e5e72cb7bb7dc150c250e22106
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 class MainPage(BasePage): 
-
-
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
-=======
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import pytest
-import time
-from selenium.common.exceptions import NoSuchElementException
+        
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
 
-link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
->>>>>>> 033e072739ada8e5e72cb7bb7dc150c250e22106
